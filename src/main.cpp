9c6c2fd7c400a78df3cd724e198357cfe6bc5ce0
iceman2146 <iceman2146@mail.ru>
2023-09-09 02:41:16 +0300

    add city time

--- src/main.cpp
+++ src/main.cpp
@@ -14,4 +14,10 @@ int main(int argc, char *argv[])
     wxPuts(wxT("дата1: ")+date1);
     wxPuts(wxT("дата2: ")+date2);
     wxPuts(wxT("дата3: ")+date3);
+
+    wxPrintf( wxT(" Tokyo: %s\n"), now.Format(wxT("%a %H:%M:%S"), wxDateTime::GMT9).c_str() );
+    wxPrintf( wxT(" Moscow: %s\n"), now.Format(wxT("%a %H:%M:%S"), wxDateTime::MSK) .c_str() );
+    wxPrintf( wxT(" Budapest: %s\n"), now.Format(wxT("%a %H:%M:%S"), wxDateTime::CEST).c_str() );
+    wxPrintf( wxT(" London: %s\n"), now.Format(wxT("%a %H:%M:%S"), wxDateTime::WEST).c_str() );
+    wxPrintf( wxT(" NewYork: %s\n"), now.Format(wxT("%a %H:%M:%S"), wxDateTime::EDT).c_str() );
 }
