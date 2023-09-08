f84cbeb5db0a5f54de243f592b5b8848389ba9dc
iceman2146 <iceman2146@mail.ru>
2023-09-09 02:21:31 +0300

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,87 +1,14 @@
-// wxWidgets "Hello World" Program
- 
-// For compilers that support precompilation, includes "wx/wx.h".
-#include <wx/wxprec.h>
- 
-#ifndef WX_PRECOMP
-    #include <wx/wx.h>
-#endif
- 
-class MyApp : public wxApp
+#include <iostream>
+#include <wx/chartype.h>
+#include <wx/string.h>
+#include <wx/crt.h>
+
+int main(int argc, char *argv[])
 {
-public:
-    virtual bool OnInit();
-};
- 
-class MyFrame : public wxFrame
-{
-public:
-    MyFrame();
- 
-private:
-    void OnHello(wxCommandEvent& event);
-    void OnMenuClose(wxCommandEvent& event);
-    void OnExit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
-};
- 
-enum
-{
-    ID_Hello = 1,
-    ID_bye=2
-};
- 
-wxIMPLEMENT_APP(MyApp);
- 
-bool MyApp::OnInit()
-{
-    MyFrame *frame = new MyFrame();
-    frame->Show(true);
-    return true;
-}
- 
-MyFrame::MyFrame()
-    : wxFrame(NULL, wxID_ANY, L"ПриветМир")
-{
-    wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, L"&ПОМОГи...\tCtrl-H",
-                     L"Не неужны нам бабьи сиси, мы фанаты эсидиси");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
- 
-    wxMenu *menuHelp = new wxMenu;
-    menuHelp->Append(wxID_ABOUT);
- 
-    wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
-    menuBar->Append(menuHelp, "&Help");
- 
-    SetMenuBar( menuBar );
- 
-    CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
- 
-    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
-    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
-    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
-    Bind(wxEVT_MENU,&MyFrame::OnMenuClose,this,ID_bye);
-}
-void MyFrame::OnMenuClose(wxCommandEvent & event)
-{
-     wxLogMessage("Bye!");
-} 
-void MyFrame::OnExit(wxCommandEvent& event)
-{
-    Close(true);
-}
- 
-void MyFrame::OnAbout(wxCommandEvent& event)
-{
-    wxMessageBox("This is a wxWidgets Hello World example",
-                 "About Hello World", wxOK | wxICON_INFORMATION);
-}
- 
-void MyFrame::OnHello(wxCommandEvent& event)
-{
-    wxLogMessage("Hello world from wxWidgets!");
+    setlocale(LC_ALL,"Russian");
+    wxPuts(wxT("вы все говно"));
+    //wxPuts-вывод в консоль
+    //Для работы текстом не GUI мы текст оборачиваем в хелпер wxT().
+
+    return 0;
 }
