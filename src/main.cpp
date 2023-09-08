57f1ecb1b10ef6e0e65f448a5f52474a2a980b97
iceman2146 <iceman2146@mail.ru>
2023-09-09 02:33:46 +0300

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,22 @@
+#ifdef __unix__                    /* __unix__ is usually defined by compilers targeting Unix systems */
+
+    #define OS_Windows 0
+    #include <unistd.h>
+    #include <stdlib.h>
+    #include <stdio.h>
+    #include <string.h>
+
+#elif defined(_WIN32) || defined(WIN32)     /* _Win32 is usually defined by compilers targeting 32 or   64 bit Windows systems */
+
+    #define OS_Windows 1
+    #include <windows.h>
+    #include <stdio.h>
+    #include <tchar.h>
+    #define DIV 1048576
+    #define WIDTH 7
+
+#endif
+
 #include <iostream>
 #include <wx/chartype.h>
 #include <wx/string.h>
@@ -9,6 +28,12 @@ int main(int argc, char *argv[])
     wxPuts(wxT("вы все говно"));
     //wxPuts-вывод в консоль
     //Для работы текстом не GUI мы текст оборачиваем в хелпер wxT().
-
+    wxString str1 = wxT("ваша");
+    wxString str2 = wxT("система");
+    wxString str3 = wxT("заражена операционной системой ");
+    wxString str4 = wxT(" ");
+    str4=(OS_Windows==0)?"Linux":"Windows";
+    wxString str = str1 + wxT(" ") + str2 + wxT(" ") + str3+str4;
+    wxPuts(str);
     return 0;
 }
